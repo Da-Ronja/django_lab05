f00e956809b2d07d8d70991e432ac1568df3aa5c
Silvia <trollunge86@gmail.com>
2024-11-14 13:58:00 +0100

    Created view and template for users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the users index.")
+
+def user_list(request):
+    user_list = User.objects.order_by('last_name')
+    user_dict = {'users': user_list}
+    return render(request, 'users/user_list.html', context=user_dict)
